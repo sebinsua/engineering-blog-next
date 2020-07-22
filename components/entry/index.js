65f76bacc364d1774133bed513884df6ee1c70db
Seb Insua <me@sebinsua.com>
2020-07-22 19:52:29 +0100

    Remove useEmoji prefixer

--- components/entry/index.js
+++ components/entry/index.js
@@ -1,5 +1,5 @@
 /* @jsx jsx */
-import { memo, useState } from 'react'
+import { memo } from 'react'
 import { jsx, Styled } from 'theme-ui'
 
 import Link from '@components/link'
@@ -8,32 +8,13 @@ const categoryEmojis = {}
 
 const fallbackEmojis = []
 
-const useEmoji = category => {
-  const [diceRoll] = useState(Math.random())
-  const emoji = category
-    ? categoryEmojis[category]
-    : fallbackEmojis[Math.round(diceRoll * (fallbackEmojis.length - 1))]
-
-  return (
-    <span role="img" aria-label={category} title={category}>
-      {emoji}
-    </span>
-  )
-}
-
 const Entry = ({ title, href, as, category, date, description }) => {
-  const emoji = useEmoji(category)
-
   return (
     <li sx={{ mb: 4 }}>
       <header>
         <Styled.h3 sx={{ mb: 0 }}>
           <Link passHref href={href} as={as} external={!as} title={`${title}`}>
-            <a sx={{ textDecoration: 'none', color: 'primary' }}>
-              {' '}
-              {emoji}
-              {title}
-            </a>
+            <a sx={{ textDecoration: 'none', color: 'primary' }}>{title}</a>
           </Link>
         </Styled.h3>
         <small>{date}</small>
